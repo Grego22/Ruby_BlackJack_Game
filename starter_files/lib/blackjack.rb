89b2f1a11801dc32061bc17bf3ff77108cd489dd
Greg Garville Jr <gjgarvil@gmail.com>
2017-08-19 13:33:25 -0400

    writing initialize method

--- starter_files/lib/blackjack.rb
+++ starter_files/lib/blackjack.rb
@@ -1,4 +1,8 @@
+Suits = ["Clubs","Hearts","Spades","Diamonds"]
+Ranks = ["1","2","3","4","5","6","7","8","9","10","11","12","13"]
+
 class BlackJack
   def initialize(argument)
     @argument = argument
   end
+end
