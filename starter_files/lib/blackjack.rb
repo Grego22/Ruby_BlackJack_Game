0da53239fdba2d28383ddbd33a8a3e8b7d5e90d3
Greg Garville Jr <gjgarvil@gmail.com>
2017-08-20 17:38:14 -0400

    writng methods

--- starter_files/lib/blackjack.rb
+++ starter_files/lib/blackjack.rb
@@ -3,19 +3,26 @@ Ranks = ["1","2","3","4","5","6","7","8","9","10","11","12","13"]
 
 class Card
   attr_reader :suit, :rank
-    def initialize(suit, rank)
-    @suit = suit
-    @rank = rank
-    puts Suits
-    puts Ranks
-end
+  def initialize(suit, rank)
+      @suit = suit
+      @rank = rank
+      puts Suits
+      puts Ranks
+  end
 
 def value
-  return 10 if @value = "J"
+  return 10 if @value == ["J", "Q", "K"]
   return @value
+  
 end
 
-puts rand(Suits)
-puts rand(Ranks)
 
+# puts rand(Suits)
+# puts rand(Ranks)
+
+end
+
+class Deck
+  def self.shuffle_cards
+end
 end
