c2743812660d8aa1d0b5302dfc91dc503b44ce48
Greg Garville Jr <gjgarvil@gmail.com>
2017-08-23 22:12:33 -0400

    moding classes

--- starter_files/lib/card.rb
+++ starter_files/lib/card.rb
@@ -1,19 +1,16 @@
 Suits = ["Clubs","Hearts","Spades","Diamonds"]
 Ranks = ["1","2","3","4","5","6","7","8","9","10","11","12","13"]
 
-class Card
-  attr_reader :suit, :rank
-  def initialize(suit, rank)
-      @suit = suit
-      @rank = rank
-      puts Suits
-      puts Ranks
+attr_reader :suit, :value
+  def initialize(suit, value)
+    @suit = suit
+    @value = value
   end
 
 def value
   return 10 if @value == ["J", "Q", "K"]
   return @value
-  
+
 end
 
 
