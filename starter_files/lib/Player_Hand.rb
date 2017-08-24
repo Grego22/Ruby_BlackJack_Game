8abe26c7232415a4dadf3bbe33b008ea17fd77b7
Greg Garville Jr <gjgarvil@gmail.com>
2017-08-24 10:03:35 -0400

    working deck class

---
+++ starter_files/lib/Player_Hand.rb
@@ -0,0 +1,6 @@
+class Player_Hand
+  attr_reader :cards
+  def initialize(argument)
+     @cards = []
+  end
+end
--- starter_files/lib/card.rb
+++ starter_files/lib/card.rb
@@ -1,6 +1,7 @@
 Suits = ["Clubs","Hearts","Spades","Diamonds"]
 Ranks = ["1","2","3","4","5","6","7","8","9","10","11","12","13"]
 
+class Card
 attr_reader :suit, :value
   def initialize(suit, value)
     @suit = suit
@@ -17,9 +18,4 @@ end
 # puts rand(Suits)
 # puts rand(Ranks)
 
-end
-
-class Deck
-  def self.shuffle_cards
-end
 end
